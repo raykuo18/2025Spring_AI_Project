124301c1f3759164faf418c42f7a2042eb270e0e
Shang-Jui Kuo <b08901174@ntu.edu.tw>
2025-05-07 04:40:23 -0400

    update

---
+++ training_data/organize_phase2_data.sh
@@ -0,0 +1,9 @@
+python patch_phase2_data.py \
+    --input_folder_base phase2/train \
+    --output_folder_base phase2/train_corrected
+python patch_phase2_data.py \
+    --input_folder_base phase2/val \
+    --output_folder_base phase2/val_corrected
+python patch_phase2_data.py \
+    --input_folder_base phase2/test \
+    --output_folder_base phase2/test_corrected
